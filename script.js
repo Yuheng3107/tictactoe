89a89c109fe699357735ccbcebec6e3363aa62fc
Kuang Yu Heng <yuheng3107@gmail.com>
2023-02-09 18:47:50 +0800

    Made rudimentary version of tictactoe

--- script.js
+++ script.js
@@ -1,16 +1,99 @@
+const symbolButtons = [...document.querySelectorAll('.symbol')];
+const startingSymbols = document.querySelector('.starting-symbols');
+
+
+symbolButtons.forEach(button => {
+    button.addEventListener('click', (e) => {
+        // Initialises gameboard
+        gameboard.initialise()
+        // Sets starting symbol
+        gameboard.setStartingSymbol(e.target.value);
+        startingSymbols.style.display = "none"
+    });
+});
+
+
 let gameboard = function() {
     // gameboard module
+    let board;
+    let spaces = [...document.querySelectorAll('.space')];
+    let currentSymbol;
+
+    spaces.forEach(space => {
+        space.addEventListener('click', (e) => {
+            // checks that game has started
+            if (!board) {
+                window.alert("Please chose either X or O");
+            }
+            else if (e.target.innerText == "") {
+            // checks that position is not occupied
+            // passes index to makeMove function
+            gameboard.makeMove(spaces.indexOf(e.target));
+            }
+            
+        });
+    });
+
     const initialise = () => {
-        let row = [null, null, null];
-        let board = [];
+        board = [
+            [null, null, null],
+            [null, null, null],
+            [null, null, null]
+        ];
+    }
+
+    function displayGrid() {
         for (let i = 0; i < 3; i++) {
-            board.push(row);
+            for (let j = 0; j < 3; j++) {
+                if (board[i][j] != null) {
+                    spaces[i*3+j].innerText = board[i][j];
+                }
+            }
         }
     }
 
-    
+    function setStartingSymbol(symbol) {
+        currentSymbol = symbol;
+    }
 
-    return {initialise,}
+    function makeMove(index) {
+        // activates when the space is clicked, taking the index of the space that was clicked
+        // first we place the new move that was made
+        
+        let [i, j] = indextoArrayIndices(index);
+        board[i][j] = currentSymbol;
+        // then display it
+        gameboard.displayGrid();
+        // check if anyone won
+        checkWinner();
+        // then we change the currentSymbol
+        currentSymbol = (currentSymbol == "X") ? "O": "X";
+    }
+
+    function indextoArrayIndices(index) {
+        return [Math.floor(index/3), index % 3];
+    }
+
+    function checkWinner() {
+        
+        for (let i = 0; i < 3; i++) {
+            // checks for rows and columns
+            if ((board[i][0] === board[i][1]) && (board[i][1] === board[i][2]) && board[i][0] != null) displayWinner();
+            if ((board[0][i] === board[1][i]) && (board[1][i] === board[2][i]) && board[0][i] !== null) displayWinner();
+        }
+        // so that no need check null for diagonals
+        if (board[1][1] === null) return;
+        // checking left diagonal
+        if ((board[0][0] === board[1][1]) && (board[1][1] === board[2][2])) displayWinner();
+        if ((board[0][2] === board[1][1]) && (board[1][1] === board[2][0])) displayWinner();
+    }
+
+    function displayWinner() {
+        // TODO
+        window.alert(`${currentSymbol} is the winner`);
+    }
+
+    return {initialise, displayGrid, setStartingSymbol, makeMove}
 
     
 
@@ -18,5 +101,6 @@ let gameboard = function() {
 
 
 function player() {
-
+// TODO
 }
+
