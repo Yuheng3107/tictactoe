9702f7cab4ca1ab13538451338d016b2477e8690
Kuang Yu Heng <yuheng3107@gmail.com>
2023-02-09 15:57:23 +0800

    Made tictactoe board

---
+++ script.js
@@ -0,0 +1,22 @@
+let gameboard = function() {
+    // gameboard module
+    const initialise = () => {
+        let row = [null, null, null];
+        let board = [];
+        for (let i = 0; i < 3; i++) {
+            board.push(row);
+        }
+    }
+
+    
+
+    return {initialise,}
+
+    
+
+}();
+
+
+function player() {
+
+}
