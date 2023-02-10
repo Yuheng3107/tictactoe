d8be8ad5090e6722ad45d1603b0fe04315107c25
Kuang Yu Heng <yuheng3107@gmail.com>
2023-02-10 17:42:52 +0800

    Added form (only css and html) to get player names

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ let gameboard = function() {
             else if (e.target.innerText == "") {
             // checks that position is not occupied
             // passes index to makeMove function
-            gameboard.makeMove(spaces.indexOf(e.target));
+            makeMove(spaces.indexOf(e.target));
             }
             
         });
@@ -48,6 +48,7 @@ let gameboard = function() {
                 if (board[i][j] != null) {
                     spaces[i*3+j].innerText = board[i][j];
                 }
+                else spaces[i*3+j].innerText = "";
             }
         }
     }
@@ -63,7 +64,7 @@ let gameboard = function() {
         let [i, j] = indextoArrayIndices(index);
         board[i][j] = currentSymbol;
         // then display it
-        gameboard.displayGrid();
+        displayGrid();
         // check if anyone won
         checkWinner();
         // then we change the currentSymbol
@@ -79,28 +80,41 @@ let gameboard = function() {
         for (let i = 0; i < 3; i++) {
             // checks for rows and columns
             if ((board[i][0] === board[i][1]) && (board[i][1] === board[i][2]) && board[i][0] != null) displayWinner();
-            if ((board[0][i] === board[1][i]) && (board[1][i] === board[2][i]) && board[0][i] !== null) displayWinner();
+            else if ((board[0][i] === board[1][i]) && (board[1][i] === board[2][i]) && board[0][i] !== null) displayWinner();
         }
         // so that no need check null for diagonals
         if (board[1][1] === null) return;
         // checking left diagonal
         if ((board[0][0] === board[1][1]) && (board[1][1] === board[2][2])) displayWinner();
-        if ((board[0][2] === board[1][1]) && (board[1][1] === board[2][0])) displayWinner();
+        else if ((board[0][2] === board[1][1]) && (board[1][1] === board[2][0])) displayWinner();
+
+        for (let i = 0; i < 3; i++) {
+            for (let j = 0; j < 3; j++) {
+                if (board[i][j] == null) return;
+            }
+        }
+        window.alert("Tie.");
+        initialise();
+        displayGrid();
     }
 
     function displayWinner() {
         // TODO
         window.alert(`${currentSymbol} is the winner`);
+        initialise();
+        displayGrid();
     }
 
-    return {initialise, displayGrid, setStartingSymbol, makeMove}
+    return {initialise, setStartingSymbol}
 
     
 
 }();
 
 
-function player() {
+function player(name) {
 // TODO
+let playerName = name;
+
 }
 
